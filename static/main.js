2ea9c14b1f65db73f67dc7c0e4d95dfeebfdc382
dhaval1624 <parmardhaval375@gmail.com>
2021-12-26 22:01:20 +0530

    try peer

--- static/main.js
+++ static/main.js
@@ -14,8 +14,9 @@ window.onload = () => { // When Window load
 };
 
 var peer = new Peer(undefined, { // Now with our peer server up an running, let's connect our cient peer js to ther server
+    path: '/peerjs',
     secure:true,
-    host: "https://zeem.herokuapp.com",
+    host: "https://zeem.herokuapp.com/",
     port: 443,
 });
 
